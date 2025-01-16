74e8359d97353236ef04e45e1bced02fbf77ca67
song <2171457@hansung.ac.kr>
2025-01-16 18:49:58 +0900

    0116

---
+++ src/main/java/kr/ac/hansung/cse/gjmarekt/GjMarektApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.hansung.cse.gjmarekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GjMarektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GjMarektApplication.class, args);
+    }
+
+}
---
+++ src/main/java/kr/ac/hansung/cse/gjmarekt/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package kr.ac.hansung.cse.gjmarekt.repository;
+
+import kr.ac.hansung.cse.gjmarekt.entity.GJUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<GJUser, Integer> {
+    Optional<GJUser> findByEmail(String email);
+}
