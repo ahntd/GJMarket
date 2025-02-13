3c002c8ee7ecb88fddd68fd7d3ac35bac02b2129
song <2171457@hansung.ac.kr>
2025-02-13 20:11:10 +0900

    CORS설정

--- src/main/java/kr/ac/hansung/cse/gjmarekt/config/WebConfig.java
+++ src/main/java/kr/ac/hansung/cse/gjmarekt/config/WebConfig.java
@@ -2,6 +2,7 @@ package kr.ac.hansung.cse.gjmarekt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.extras.springsecurity6.dialect.SpringSecurityDialect;
@@ -16,6 +17,17 @@ public class WebConfig implements WebMvcConfigurer
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/admin/home").setViewName("adminhome");
         registry.addViewController("/accessDenied").setViewName("403");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 
     @Bean
